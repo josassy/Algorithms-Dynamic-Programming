ef84968b338b5925a01b94b7145b9e0d24dfcab3
Josiah Lansford <redrobbin99@gmail.com>
2020-10-05 00:38:36 -0400

    formatting changes

--- change/Utilities.java
+++ change/Utilities.java
@@ -4,15 +4,17 @@ public class Utilities {
   // print a given solution in the specified format
   public static String SolutionToString(Solution s, Integer[] denoms) {
     String result = "";
-    for (int i = 0; i < denoms.length; i++) {
+    // loop from highest to lowest
+    for (int i = denoms.length - 1; i >= 0; i--) {
       // add denom and corresponding count, if > 0
       if (s.denoms[i] > 0) {
-        result += denoms[i] + ":" + s.denoms[i];
-        
-        // if more denoms to print, add a space
-        if (i < denoms.length - 1) {
+
+        // don't print a space if the string is empty
+        if (result != "") {
           result += " ";
         }
+
+        result += denoms[i] + ":" + s.denoms[i];
       }
     }
     return result;
