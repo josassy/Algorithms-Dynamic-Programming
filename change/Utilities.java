9fd30fe6a1f5c22bc6052aa6a5e586d6eb6c809c
Josiah Lansford <redrobbin99@gmail.com>
2020-10-05 00:30:48 -0400

    don't print empty denoms

--- change/Utilities.java
+++ change/Utilities.java
@@ -5,12 +5,14 @@ public class Utilities {
   public static String SolutionToString(Solution s, Integer[] denoms) {
     String result = "";
     for (int i = 0; i < denoms.length; i++) {
-      // add denom and corresponding count
-      result += denoms[i] + ":" + s.denoms[i];
-      
-      // if more denoms to print, add a space
-      if (i < denoms.length - 1) {
-        result += " ";
+      // add denom and corresponding count, if > 0
+      if (s.denoms[i] > 0) {
+        result += denoms[i] + ":" + s.denoms[i];
+        
+        // if more denoms to print, add a space
+        if (i < denoms.length - 1) {
+          result += " ";
+        }
       }
     }
     return result;
